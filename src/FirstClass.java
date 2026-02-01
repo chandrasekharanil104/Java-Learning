193332f3bbe03519802fc156cea4dd7829f37d91
Chintakindi, Chandra Sekhar Anil <chandrasekharanil104@gmail.com>
2026-02-01 16:03:37 -0600

    Jenkins integration with github and triggering automatically

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -2,5 +2,6 @@ public class FirstClass {
     public static void main(String[] args){
         System.out.print("Hello Chandra Sekhar Anil Chintakindi!");
         System.out.println(" Welcome to Java Programming");
+        System.out.println("This is my first Java Program with Jenkins Integration using Webhooks on github and Jenkins");
     }
 }
